5638f5b6aaf211f368011ec47fa509ef3500335f
startdevelopfin <danieljaupont@gmail.com>
2025-03-03 15:56:10 -0500

    update static.yml workflow

--- Sources/Site.swift
+++ Sources/Site.swift
@@ -34,27 +34,5 @@ struct SDFSite: Site {
         Story()
         CustomStory()
     }
-    
-    // Add a custom URL transformer that all components can use
-    func transformURL(_ path: String) -> String {
-        if path.hasPrefix("http") {
-            // External URLs don't need transformation
-            return path
-        } else if path.hasPrefix("/") {
-            // Absolute paths need the basePath
-            return basePath + path
-        } else {
-            // Relative paths need a separator and basePath
-            return basePath + "/" + path
-        }
-    }
-}
-
-// Then use an extension on components that need URL handling
-extension Link {
-    // Create a convenience initializer that uses the site's URL transformer
-    init(text: String, to path: String, in site: SDFSite) {
-        self.init(text, target: site.transformURL(path))
-    }
 }
 
