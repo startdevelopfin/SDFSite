ac2f45bd31a317eb11468e90641c640cf4d46e20
startdevelopfin <danieljaupont@gmail.com>
2025-03-04 02:55:45 -0500

    try 999

--- Sources/Components/NavBar.swift
+++ Sources/Components/NavBar.swift
@@ -9,28 +9,9 @@ import Foundation
 import Ignite
 
 /// An example navigation bar, demonstrating how to create reusable components.
-//struct NavBar: HTML {
-//    var body: some HTML {
-//        NavigationBar(logo: "Start. Develop. Finish.") {
-//            Dropdown("App Store") {
-//                Link("By Rule", target: "https://byruleapp.com")
-//            }
-//            
-//            Dropdown("SwiftUI 100") {
-//                Text(markdown: "[Week 1](story/swiftuiweek1)")
-//            }
-//        }
-//        .navigationItemAlignment(.trailing)
-//        .navigationBarStyle(.dark)
-//        .background(.black)
-//        .position(.fixedTop)
-//    }
-//}
 struct NavBar: HTML {
     var body: some HTML {
         NavigationBar(logo: "Start. Develop. Finish.") {
-//            Link("", target: "/SDFSite/")  // Ensure it's pointing to /SDFSite/
-            
             Dropdown("App Store") {
                 Link("By Rule", target: "https://byruleapp.com")
             }
